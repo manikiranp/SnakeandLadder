c3f4259207840c4f2a2201390a927ee84a2b6fe8
manikiranp <manikiranp3@gmail.com>
2020-11-11 15:45:37 +0530

    Welcome to snake and ladder game

--- src/snakeladder/SnakeLadder.java
+++ src/snakeladder/SnakeLadder.java
@@ -10,16 +10,11 @@ public class SnakeLadder {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game:\n");
-		position();
 
 	}
 
 
 
-	private static void position() {
-		System.out.println("Player1 position:"+pos);
-		System.out.println("Player2 position:"+pos);
-		
-	}
+
 
 }
