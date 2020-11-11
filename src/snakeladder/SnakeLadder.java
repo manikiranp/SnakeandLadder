ffc26c8f75f4280a27b523720752b8fdf018bb9e
manikiranp <manikiranp3@gmail.com>
2020-11-11 16:44:16 +0530

    Player position updated based on dice roll value

--- src/snakeladder/SnakeLadder.java
+++ src/snakeladder/SnakeLadder.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 public class SnakeLadder {
 	
-	static int p1=0; 
+	static int p1=0;
+	static int dice=0;
+	
 	static final int start=0;
 	static final int end=100;
 	
@@ -18,11 +20,15 @@ public class SnakeLadder {
 	}
 	private static void diceroll() {
 		Random randnum = new Random();
-		int dice = randnum.nextInt(6)+1;
-		System.out.println("Rolling Dice...\nDice Number :"+dice);
+		dice = randnum.nextInt(6)+1;
+		System.out.println("Rolling Dice...\nDice Number :"+ dice);
+		position();
+		
 	}
 	private static void position() {
-		System.out.println("Player position:"+ start);
+		p1=p1+dice;
+		System.out.println("Player position:"+ p1);
+		
 		
 	}
 
