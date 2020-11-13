821584fdd5023f6d7d56992ad2967d023f1d92ee
manikiranp <manikiranp3@gmail.com>
2020-11-13 17:06:36 +0530

    code refactored and winning person displayed

--- src/snakeladder/SnakeLadder.java
+++ src/snakeladder/SnakeLadder.java
@@ -5,10 +5,8 @@ import java.util.Random;
 public class SnakeLadder {
 	
 	static int p1=0, p2=0;
-	static int dice=0;;
-	static int opt=0;
+	static int dice=0;
 	static int k=0;
-	
 	static int count=0;
 	static final int start=0;
 	static final int end=100;
@@ -39,8 +37,6 @@ public class SnakeLadder {
 		}
 		current=-current;
 		}
-			
-	
 		
 	}
 	private static int option(int dice) {
@@ -49,47 +45,27 @@ public class SnakeLadder {
 		switch (num) {
 		case 1: 
 			System.out.println("Reached Ladder");
-
 			k=dice;
-			if (p1>100) {
-				System.out.println("Exceeeding 100");
-				p1=p1-dice;
-			}
-				else if (p1==100) {
-				System.out.println("Reached 100");
-				break;
-			}
 			break;
 
 		case 2: 
 			System.out.println("Snake Encountered");
-
 			k=-dice;
-			if (p1>100) {
-				System.out.println("Exceeeding 100");
-				p1=p1-dice;
-			}
-			else if (p1==100) {
-				System.out.println("Reached 100");
-				
-			}
 			break;
-			
 		
 		case 0: 
 			System.out.println("Same position");
-
 			k=0;
 			break;
 		}
 		return k;
 	}
+	
 	private static int diceroll() {
 		dice=0;
 		dice = randnum.nextInt(6)+1;
 		System.out.println("Rolling Dice...:"+ dice);
 		count++;
-		
 		return dice;
 
 		
@@ -98,39 +74,37 @@ public class SnakeLadder {
 		
 		if (current<0) {
 			System.out.println("---Updated Player1 position:"+p1);
+			if (p1==100 ) {
+				System.out.println("\nPlayer1 won the game!!\n");
+				System.out.println("Total number of times die rolled:"+count);
+				System.exit(0); 
+				}
 		}
 		else {
 			System.out.println("---Updated Player2 position:"+p2);
+			if (p2==100) {
+				System.out.println("\nPlayer2 won the game!!\n");
+				System.out.println("Total number of times die rolled: "+count);
+				System.exit(0); 
+				}
 		}
-		if (p1==100 || p2==100) {
-			System.out.println("Reached winning position:"+ end);
-			System.out.println("Total number of times die rolled:"+count);
-			System.exit(0);
-		}		
+			
 	}
 	
 	private static int calcplayerpos(int player, int dice, int k) {
-//		diceroll();
-//		option(dice);
+
 		player = player+dice+k;
-		System.out.println(player+"-"+dice+"-"+k);
+//		System.out.println(player+"-"+dice+"-"+k);
 		if (player>end) {
-			player = player - dice-k;
+			player = player-dice-k;
 				
 		}
 		if (player<0) {
-			player=0;
-			
+			player=0;	
 			}
-		if (player==100 && player==p1) {
-			System.out.println("Player1 has won***:");
-			
-		}
-		if (player==100 && player==p2) {
-			System.out.println("Player2 has won***:");
-			
 		
-		}
+		
+		
 		return player;
 		
 		
